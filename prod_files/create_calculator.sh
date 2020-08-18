7300753c3b3ef23e53fc2fd4a7a41acc7044497f
dschandl <dschandl@sfu.ca>
2020-08-17 19:25:34 -0700

    Small changes to scripts.

--- prod_files/create_calculator.sh
+++ prod_files/create_calculator.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker volume create --name="calculator-logs"
+export COMPOSE_PROJECT_NAME="dschandl"
+docker volume create --name="${COMPOSE_PROJECT_NAME}_calculator-logs"
 docker-compose -f prod_files/docker-compose.yml up
